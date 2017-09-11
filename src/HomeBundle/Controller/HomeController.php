29fcf50490a3014cc5395ee2cce6c7945456ab66
Garali Karim <garalimedkarim4@gmail.com>
2017-09-11 22:15:17 +0100

    integration home complete

--- src/HomeBundle/Controller/HomeController.php
+++ src/HomeBundle/Controller/HomeController.php
@@ -4,10 +4,18 @@ namespace HomeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use FOS\UserBundle\Model\UserInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
 class HomeController extends Controller
 {
     public function homeAction()
     {
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }		
+		
         return $this->render('HomeBundle:Default:home.html.twig');
     }
 }
