5b7e8ad92e03e75b6e53a370232d37c1b83ba210
Garali Karim <garalimedkarim4@gmail.com>
2017-09-27 12:01:20 +0100

    styling not connected mode

--- src/HomeBundle/Form/FournisseurType.php
+++ src/HomeBundle/Form/FournisseurType.php
@@ -13,7 +13,33 @@ class FournisseurType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nomFournisseur')->add('adresse')->add('ville')->add('codeTva')->add('tel')->add('tel2')->add('tel1')->add('fax')->add('contact')->add('gsm')->add('contact1')->add('gsm1')->add('gsm2')->add('contact2')->add('contact3')->add('gsm3')->add('echange')->add('modereg')->add('mail')->add('postal')->add('frs')->add('famille')->add('remise')->add('solde')->add('timbre')->add('idfam');
+        $builder
+			->add('nomFournisseur')
+			->add('adresse')
+			->add('ville')
+			->add('codeTva')
+			->add('tel')
+			->add('tel2')
+			->add('tel1')
+			->add('fax')
+			->add('contact')
+			->add('gsm')
+			->add('contact1')
+			->add('gsm1')
+			->add('gsm2')
+			->add('contact2')
+			->add('contact3')
+			->add('gsm3')
+			->add('echange')
+			->add('modereg')
+			->add('mail')
+			->add('postal')
+			->add('frs')
+			->add('famille')
+			->add('remise')
+			->add('solde')
+			->add('timbre')
+			->add('idfam');
     }
     
     /**
